cd62ac6529a8d307c621a8d97a07569b79122923
Luiz Miguel <luizmiguelw2005@gmail.com>
2026-02-23 11:10:10 -0300

    chore: pausa para reestruturacao arquitetural em 3 camadas (DLLs)

---
+++ build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/.qt/appSGPL_qml.cmake
@@ -0,0 +1,9 @@
+
+set(target "appSGPL")
+set(working_dir "C:/SGPL/SGPL")
+set(src_and_dest_list
+    "C:/SGPL/SGPL/Main.qml"
+    "C:/SGPL/SGPL/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/SGPL/Main.qml"
+
+)
+set(timestamp_file "C:/SGPL/SGPL/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/.qt/appSGPL_qml.txt")
